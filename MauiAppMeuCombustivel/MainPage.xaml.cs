3783fd2c9e818d4d5fc1793878ae36eebfb9dea9
Araujo <jrl.araujo@hotmail.com>
2024-11-18 09:36:42 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Captura os valores da marca, modelo, etanol e gasolina
+                string marca = txt_marca.Text;
+                string modelo = txt_modelo.Text;
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                // Verifica qual combustível compensa mais
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = $"O etanol está compensando para o veículo {marca} {modelo}.";
+                }
+                else
+                {
+                    msg = $"A gasolina está compensando para o veículo {marca} {modelo}.";
+                }
+
+                // Exibe o resultado em um alerta
+                DisplayAlert("Calculado", msg, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+        }
+    }
+}
